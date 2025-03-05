35b3de3ed4a893f64c308a406baa4a298694c6d0
lmolnar <lukemmolnar@gmail.com>
2025-03-05 16:26:08 -0500

    ci test

--- src/components/EmptyState.jsx
+++ src/components/EmptyState.jsx
@@ -6,7 +6,7 @@ export const EmptyState = () => (
       <h2 className="text-xl font-semibold text-teal-400 mb-4">SLUMNET</h2>
       <p className="text-teal-400 mb-4">Bub boils the seed!</p>
       <div className="text-teal-50 bg-stone-950 p-2 font-mono text-sm">
-        <p>Keyboard shorties:</p>
+        <p>Keyboard shortiwadaes:</p>
         <p>Ctrl+Enter - Create/Split Vertical</p>
         <p>Ctrl+Shift+Enter - Split Horizontal</p>
         <p>Ctrl+Backspace - Close Window</p>
